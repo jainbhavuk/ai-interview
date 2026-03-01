7dd6acba53baea0b4a5b3585614000200ae00642
bhavuk.jain <bhavuk.jain@extramarks.com>
2026-03-01 19:50:06 +0530

    update model

--- backend/config/llmConfig.js
+++ backend/config/llmConfig.js
@@ -3,7 +3,7 @@ import { ChatOpenAI } from '@langchain/openai';
 function createLLM() {
   return new ChatOpenAI({
     apiKey: process.env.OPENAI_API_KEY,
-    model: process.env.MODEL || 'gpt-4o',
+    model: 'gpt-4.1',
     maxTokens: 10000,
     temperature: 0.6,
   });
